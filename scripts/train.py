e495f81f0c1ec420b06a0ef286fe065d12205b91
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2025-12-06 00:24:36 +0100

    training pipelines

--- scripts/train.py
+++ scripts/train.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import random
+from pathlib import Path
 from src.utils import inspect_policy
 from src.training import training
 
@@ -16,47 +16,17 @@ if __name__ == '__main__':
     _RNG = random.Random(42)
     _CONFIG = {"learning_rate": 0.1,
                "temperature": 1.0,
-               "min_logit": -10,
-               "momentum": 0.9,
-               "n_epochs": 10_000,
-               "n_cycles": 100}
+               "init_range": 0.1,
+               "logit_range": 20,
+               "momentum": 0.9,  # on accumulated rewards and counts
+               "damping": 0.5,   # on logits
+               "n_epochs": 5_000,
+               "n_cycles": 50}
 
-    # ------------------- TRAINING ---------------------
+    # ---------------- TRAINING PIPELINE ----------------
 
-    _CONFIG["learning_rate"] = 0.1
-    _CONFIG["n_cycles"] = 100
-    _CONFIG["n_epochs"] = 20_000
-    _CONFIG["min_logit"] = -4
     training(file_path=_POLICY_PATH, config=_CONFIG, rng=_RNG)
 
-    _CONFIG["learning_rate"] = 0.05
-    _CONFIG["n_cycles"] = 100
-    _CONFIG["n_epochs"] = 20_000
-    _CONFIG["min_logit"] = -10
-    training(file_path=_POLICY_PATH, config=_CONFIG, rng=_RNG)
+    # --------------- INSPECT THE RESULTS ---------------
 
-    # _CONFIG["n_epochs"] = 10_000
-    # _CONFIG["n_cycles"] = 200
-    # _CONFIG["min_logit"] = -4
-    # training(file_path=_POLICY_PATH, config=_CONFIG, rng=_RNG)
-    #
-    # _CONFIG["min_logit"] = -10
-    # training(file_path=_POLICY_PATH, config=_CONFIG, rng=_RNG)
-
-    # _CONFIG["n_epochs"] = 10_000
-    # _CONFIG["min_logit"] = -2
-    # training(file_path=_POLICY_PATH, config=_CONFIG, rng=_RNG)
-    #
-    # _CONFIG["min_logit"] = -3
-    # training(file_path=_POLICY_PATH, config=_CONFIG, rng=_RNG)
-    #
-    # _CONFIG["min_logit"] = -5
-    # training(file_path=_POLICY_PATH, config=_CONFIG, rng=_RNG)
-    #
-    # _CONFIG["n_epochs"] = 20_000
-    # _CONFIG["learning_rate"] = 0.05
-    # _CONFIG["min_logit"] = -10
-    # training(file_path=_POLICY_PATH, config=_CONFIG, rng=_RNG)
-
-    # Inspect the results
-    inspect_policy(file_path=_POLICY_PATH, show_proba=False)
+    inspect_policy(file_path=_POLICY_PATH, show_proba=True)
