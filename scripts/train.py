b61a1985b402553b548d5ac35bd86dffec753098
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2025-12-06 15:31:18 +0100

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -2,30 +2,32 @@ import random
 from pathlib import Path
 from src.utils import inspect_policy
 from src.training import training
+from src.agents.rl_agent import AgentRL
 
 
 if __name__ == '__main__':
     # ------------------ CONFIGURATION ------------------
 
     # Model info
-    _MODEL_NAME = "new"
+    _MODEL_NAME = "_test"
+    _AGENT_CLASS = AgentRL
     _POLICY_PATH = Path(f"..\\models\\{_MODEL_NAME}.json")
-    # _POLICY_PATH = Path(f"..\\models\\tournament_2025\\{_MODEL_NAME}.pkl")
+    # _POLICY_PATH = Path(f"..\\models\\tournament_2025\\{_MODEL_NAME}.json")
 
     # Config
     _RNG = random.Random(42)
-    _CONFIG = {"learning_rate": 0.1,
+    _CONFIG = {"learning_rate": 0.05,
                "temperature": 1.0,
                "init_range": 0.1,
                "logit_range": 20,
-               "momentum": 0.9,  # on accumulated rewards and counts
-               "damping": 0.5,   # on logits
-               "n_epochs": 5_000,
+               "momentum": 0.99,  # on accumulated rewards and counts
+               "damping": 1.,   # on logits
+               "n_epochs": 10_000,
                "n_cycles": 50}
 
     # ---------------- TRAINING PIPELINE ----------------
 
-    training(file_path=_POLICY_PATH, config=_CONFIG, rng=_RNG)
+    training(agent_class=_AGENT_CLASS, file_path=_POLICY_PATH, config=_CONFIG, rng=_RNG)
 
     # --------------- INSPECT THE RESULTS ---------------
 
