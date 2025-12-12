a433c273fac393de8fa12c23c2fab93410e5e68f
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2025-12-12 18:10:25 +0100

    Update inspect_policy.py

---
+++ scripts/inspect_policy.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from src.utils import inspect_policy
+
+
+if __name__ == '__main__':
+
+    # ------------------ CONFIGURATION ------------------
+    # Model info
+    _MODEL_NAME = "agentrl"
+    _POLICY_PATH = Path(f"..\\models\\{_MODEL_NAME}.json")
+    # _POLICY_PATH = Path(f"..\\models\\tournament_2025\\{_MODEL_NAME}.json")
+
+    # --------------- INSPECT THE RESULTS ---------------
+    inspect_policy(file_path=_POLICY_PATH, show_proba=False)
