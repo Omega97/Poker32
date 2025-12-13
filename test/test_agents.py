bbdea5b4d796178690e8a0a4503c319452d548c0
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2025-12-13 10:53:32 +0100

    Update test_agents.py

--- test/test_agents.py
+++ test/test_agents.py
@@ -7,17 +7,19 @@ def test_random_agent(rng=random.Random(3), verbose=True):
 
     # Init the game
     game = Poker32(rng)
-    if verbose:
-        print(game.hole_cards)
 
     # Init players
-    players = (RandomAgent(rng, verbose=verbose),
-               RandomAgent(rng, verbose=verbose))
+    players = (RandomAgent(rng, name="Agent_1", verbose=verbose),
+               RandomAgent(rng, name="Agent_2", verbose=verbose))
 
     # Play the game
-    results = game.play(players, verbose=verbose)
-    if verbose:
-        print(results)
+    for i in range(4):
+        print()
+        results = game.play(players)
+        if verbose:
+            print('\nResults:')
+            for k, v in results.items():
+                print(f"{k:>12} {v}")
 
 
 if __name__ == '__main__':
