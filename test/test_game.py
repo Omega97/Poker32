540a36fa846fb725c14fc74f58958ae0c86d4538
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2025-12-13 10:16:04 +0100

    Update test_game.py

--- test/test_game.py
+++ test/test_game.py
@@ -1,4 +1,6 @@
 # tests/test_poker32.py
+import random
+
 import src.poker32 as poker
 
 
@@ -6,21 +8,29 @@ def test_rewards():
     """Check reward logic for a few terminal nodes."""
     poker.init()
 
+    dct = [('cRRf', {"SB": "A", "BB": "K"}, {"SB": 4, "BB": -4}),
+           ('RRc', {"SB": "A", "BB": "2"}, {"SB": 8, "BB": -8}),
+           ('cc', {"SB": "K", "BB": "A"}, {"SB": -2, "BB": 2}),
+           ('cc', {"SB": "K", "BB": "K"}, {"SB": 0, "BB": 0}),
+           ('cRf', {"SB": "K", "BB": "K"},  {"SB": -2, "BB": 2})
+           ]
+
     # Quick sanity checks
-    assert poker.rewards('cRRf', holes=["A", "K"]) == (4, -4)
-    assert poker.rewards('RRc', holes=["A", "2"]) == (8, -8)
-    assert poker.rewards('cc', holes=["K", "A"]) == (-2, 2)
-    assert poker.rewards('cc', holes=["K", "K"]) == (0, 0)
-    assert poker.rewards('cRf', holes=["K", "K"]) == (-2, 2)
+    for leaf, hole_cards, rewards in dct:
+        game_rewards = poker.rewards(leaf, hole_cards)
+        if not game_rewards == rewards:
+            raise ValueError(f'{game_rewards} != {rewards}')
 
     print('test_rewards passed!')
 
 
 def test_game_initialisation():
     """Ensure the game object starts in the expected state."""
-    game = poker.Poker32()
+    rng = random.Random(0)
+    game = poker.Poker32(rng=rng)
+
     assert game.button == 0
-    assert game.hole_cards == ["A", "6"]
+    assert game.hole_cards == ["Q", "2"]
     game._init_hole_cards(holes=["Q", "Q"])
     assert game.hole_cards == ["Q", "Q"]
 
@@ -44,28 +54,27 @@ def test_game_tree():
     for leaf in leafs:
         assert leaf in poker.GAME_TREE
 
-    for k, v in poker.GAME_TREE.items():
-        print(k, v)
-
+    # for k, v in poker.GAME_TREE.items():
+    #     print(k, v)
 
 
 def test_game():
     """Ensure the game object starts in the expected state."""
     game = poker.Poker32()
     state = game._get_subjective_state(0)
-    print(state)
-    print(game.get_legal_moves())
+    # print(state)
+    # print(game.get_legal_moves())
     game.make_move('f')
     assert game._get_subjective_state(0)['branch'] == 'f'
     assert game.is_game_over() is True
-    assert game.get_relative_rewards() == (-1, 1)
+    assert game.get_rewards() == {"SB": -1, "BB": 1}
 
     print('test_game_initialisation passed!')
 
 
 if __name__ == "__main__":
-    # test_rewards()
-    # test_game_initialisation()
-    # test_degrees_of_freedom()
+    test_rewards()
+    test_game_initialisation()
+    test_degrees_of_freedom()
     test_game_tree()
-    # test_game()
+    test_game()
